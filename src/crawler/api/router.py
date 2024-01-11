696fc22bcfc9e2133307902d58b3af1cc2ee4bc2
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-10 16:03:05 -0800

    added crawler domain

---
+++ src/crawler/api/router.py
@@ -0,0 +1,8 @@
+"""All Routes."""
+from fastapi import APIRouter
+from server_config import health_router
+
+api_router = APIRouter()
+
+# Health endpoint.
+api_router.include_router(health_router, prefix="/health")
---
+++ src/crawler/server_config/__init__.py
@@ -0,0 +1,2 @@
+from server_config.settings import LOGGING_CONFIG_PATH
+from server_config.health_check import router as health_router
