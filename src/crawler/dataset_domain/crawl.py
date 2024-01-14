b9e2b2cc572228852c0fc6fe5c6132b87212bad7
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-14 15:47:58 -0800

    added final script

--- src/crawler/dataset_domain/crawl.py
+++ src/crawler/dataset_domain/crawl.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import requests
 import PyPDF2
 import docx2txt
@@ -20,7 +21,21 @@ class Crawl:
         self.starting_state = self.csv_data['state'][0]
         self.formatted_data = self.title_and_info(self.csv_data['state'][0], self.csv_data['color'][0])
         print(self.formatted_data)
+        self.initial_patterns = ['Title IX', 'Commission', 'regulations', 'Sex', 'Rights', 'Discrimination', 'Law', 'Harassment', 'Policy']
+        self.match_patterns = self.initialize_patterns()
         self.crawl()
+
+    def initialize_patterns(self):
+        final_patterns = self.initial_patterns
+        for i in range(0, len(self.initial_patterns)):
+            final_patterns.append(self.initial_patterns[i].lower())
+        return final_patterns
+
+    def ismatch_title_link_map(self, s):
+        for i in self.match_patterns:
+            if re.search(i, s):
+                return True
+        return False
 
     def clean_and_add_data(self, text_data):
         text_list = text_data.split("\n")
@@ -60,6 +75,13 @@ class Crawl:
         # logger.info("Formatted Child Data - {}".format(child_data))
         return child_data
 
+    def reject_unwanted_links(self):
+        filtered_links = []
+        for links in self.child_links:
+            if self.ismatch_title_link_map(links.text.strip()):
+                filtered_links.append(links)
+        self.child_links = filtered_links
+
     def get_child_link_data(self, link_list):
         children_data = ''
         self.child_links = []
@@ -68,6 +90,9 @@ class Crawl:
                 self.child_links.append(link)
         logger.info("Rejecting around links - {}".format(len(link_list) - len(self.child_links)))
         logger.info("Remaining Child links - {}".format(len(self.child_links)))
+        if len(self.child_links) > 20:
+            self.reject_unwanted_links()
+            logger.info("Rejecting Unwanted Child links - {}".format(len(self.child_links)))
         skip_links = ['https://disclosures.utah.gov/Search/PublicSearch?type=PCC', 'https://image.le.utah.gov/imaging/History.asp', 'https://image.le.utah.gov/imaging/bill.asp', 'https://lag.utleg.gov/audits_current.jsp', 'https://lag.utleg.gov/', 'https://lag.utleg.gov/annual_report.jsp', 'https://lag.utleg.gov/best_practices.jsp']
         for child_link in self.child_links:
             child_url = child_link.get('href')
@@ -82,8 +107,8 @@ class Crawl:
         return children_data
 
     def title_and_info(self, state, color):
-        party = 'Democratic' if color == 'blue' else 'Republic'
-        format_header = "TITLE - {} TITLE IX DOCUMENTATION\n\n{} State ({} Region)\n\n".format(state.upper(), party, color)
+        party = 'Democratic' if color == 'Blue' else 'Republic'
+        format_header = "TITLE - {} TITLE IX DOCUMENTATION\n\n{} State ({} Region) (This is crawled data)\n\n".format(state.upper(), party, color)
         return format_header
 
     def crawl(self):
@@ -99,7 +124,6 @@ class Crawl:
                     read_pdf = PyPDF2.PdfFileReader(data)
                     for page in range(read_pdf.getNumPages()):
                         self.formatted_data = self.formatted_data + read_pdf.getPage(page).extractText()
-                logger.info("Formatted Child Data - {}".format(self.formatted_data))
             elif row['type'] == 'docs':
                 file = self.url_map[row['url']]
                 self.formatted_data = self.formatted_data + docx2txt.process(file)
@@ -111,7 +135,7 @@ class Crawl:
                 link_list = soup.find_all("a")
                 child_data = self.get_child_link_data(link_list)
                 self.formatted_data = self.formatted_data + format_data + child_data
-        logger.info("Formatted Child Data - {}".format(self.formatted_data))
+        # logger.info("Formatted Child Data - {}".format(self.formatted_data))
         with open('output/{}.txt'.format(self.starting_state), "w", encoding="utf-8") as f:
             f.write(self.formatted_data)
 
