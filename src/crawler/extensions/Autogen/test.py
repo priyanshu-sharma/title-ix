b191635e26554da5872503a34beff5db40ab9226
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-13 17:50:06 -0800

    fix bugs in autogen

--- src/crawler/extensions/Autogen/test.py
+++ src/crawler/extensions/Autogen/test.py
@@ -1,42 +1,20 @@
 import autogen
 
-config_list = [
-    {
-        "api_type": "open_ai",
-        "api_base": "http://127.0.0.1:8080/v1",
-        "api_key": "NULL"
-    }
-]
+config_list = [{"base_url": "http://127.0.0.1:8080/v1", "api_key": "NULL"}]
 
-llm_config = {
-    "request_timeout": 600,
-    "seed": 42,
-    "config_list": config_list,
-    "temperature": 0
-}
-
-assistant = autogen.AssistantAgent(
-    name="assistant",
-    system_message="You are a coder specializing in Python.",
-    llm_config=llm_config
-)
-
+assistant = autogen.AssistantAgent("assistant", llm_config={"seed": 42, "config_list": config_list, "temperature": 0})
 user_proxy = autogen.UserProxyAgent(
-    name="user_proxy",
+    "user_proxy",
+    llm_config={"config_list": config_list},
     human_input_mode="NEVER",
-    max_consecutive_msg=10,
+    max_consecutive_auto_reply=5,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": "."},
-    llm_config=llm_config,
-    system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
-    Otherwise, reply CONTINUE, or the reason why the task is not solved yet"""
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
 )
-
-task = """
-write a python method to output numbers 1 to 100.
-"""
-
 user_proxy.initiate_chat(
     assistant,
-    message=task
+    message="""What date is today? Compare the year-to-date gain for META and TESLA.""",
 )
