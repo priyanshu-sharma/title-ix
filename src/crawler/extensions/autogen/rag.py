a37b36aaf6b714fd8df9717d2361f9b2e9e5a9ed
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-19 17:27:42 -0800

    fix rag

--- src/crawler/extensions/autogen/rag.py
+++ src/crawler/extensions/autogen/rag.py
@@ -1,28 +1,35 @@
 import autogen
-from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
-from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+from autogen.agentchat.contrib.retrieve_assistant_agent import \
+    RetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import \
+    RetrieveUserProxyAgent
 
-config_list = [{"base_url": "http://0.0.0.0:8000", "model": "mistral"}]
-llm_config = {
-    "cache_seed": 42,  
-    "temperature": 0,
-    "config_list": config_list,
-    "timeout": 120,
-}
 
-texas_assistant = RetrieveAssistantAgent(
-    name="assistant",
-    system_message="You are a helpful assistant, which help in determining policies, plan and other implementation details of Title IX implementation in Texas.",
-    llm_config=llm_config,
-)
+class TitleIXRag:
+    def __init__(self):
+        self.config_list = [{"base_url": "http://0.0.0.0:8000", "model": "mistral"}]
+        self.llm_config = {
+            "cache_seed": 42,  
+            "temperature": 0,
+            "config_list": self.config_list,
+            "timeout": 120,
+        }
 
-texas_ragproxyagent = RetrieveUserProxyAgent(
-    name="ragproxyagent",
-    retrieve_config={
-        "task": "qa",
-        "docs_path": "https://raw.githubusercontent.com/priyanshu-sharma/title-ix/master/src/crawler/dataset_domain/output/texas.txt?token=GHSAT0AAAAAAB3JO6UQ4NKO7LH7E5NGO5QEZNGFFJA",
-    },
-)
+        self.texas_assistant = RetrieveAssistantAgent(
+            name="assistant",
+            system_message="You are a helpful assistant, which help in summarizing policies, plan and other implementation details of Title IX implementation in Texas.",
+            llm_config=self.llm_config,
+        )
 
-texas_assistant.reset()
-texas_ragproxyagent.initiate_chat(texas_assistant, problem="How is the Title IX Implementation in Texas?")
+        self.texas_ragproxyagent = RetrieveUserProxyAgent(
+            name="ragproxyagent",
+            retrieve_config={
+                "task": "qa",
+                "must_break_at_empty_line": "True",
+                "docs_path": "https://raw.githubusercontent.com/priyanshu-sharma/title-ix/master/src/crawler/dataset_domain/output/texas.txt?token=GHSAT0AAAAAAB3JO6UQ4NKO7LH7E5NGO5QEZNGFFJA",
+            },
+        )
+
+    def chat(self, problem):
+        self.texas_assistant.reset()
+        self.texas_ragproxyagent.initiate_chat(self.texas_assistant, problem=problem)
