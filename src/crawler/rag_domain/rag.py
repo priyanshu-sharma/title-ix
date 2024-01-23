8b1aeef0620416a82b5e573566f6734f8c1cbc5d
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-22 18:39:27 -0800

    added Texas in rag

--- src/crawler/rag_domain/rag.py
+++ src/crawler/rag_domain/rag.py
@@ -7,12 +7,27 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
+class TitleRag:
+    def __init__(self):
+        llm = Ollama(model="mistral")
+        service_context = ServiceContext.from_defaults(llm=llm, embed_model="local")
+        documents = SimpleDirectoryReader("data").load_data()
+        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        self.query_engine = index.as_query_engine()
+        self.pre_evaluation()
+    
+    def pre_evaluation(self):
+        response = self.query_engine.query("Can you give me the summary of this Title IX Implemention of California?")
+        print(response)
+        response = self.query_engine.query("Can you give me the summary of this Title IX Implemention of Texas?")
+        print(response)
+        response = self.query_engine.query("How is the implementation of Title IX is different in California and Texas?")
+        print(response)
 
-llm = Ollama(model="mistral", request_timeout=30.0)
-service_context = ServiceContext.from_defaults(llm="local")
+    def evaluate(self, question):
+        response = self.query_engine.query(question)
+        print(response)
 
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+ta = TitleRag()
 
-query_engine = index.as_query_engine()
-response = query_engine.query("Can you give me the summary of this Title IX Implemention of California?")
+        
