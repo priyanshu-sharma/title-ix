bc704c22c330c8933c466bd2f43a4acb473dbb1e
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-24 14:03:50 -0800

    added storage context

--- src/crawler/rag_domain/rag.py
+++ src/crawler/rag_domain/rag.py
@@ -2,6 +2,7 @@ import json
 import logging
 import sys
 
+import chromadb
 from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
 from llama_index.embeddings import HuggingFaceEmbedding
 from llama_index.extractors import (EntityExtractor, KeywordExtractor,
@@ -10,8 +11,10 @@ from llama_index.extractors import (EntityExtractor, KeywordExtractor,
 from llama_index.ingestion import IngestionPipeline
 from llama_index.llms import Ollama
 from llama_index.node_parser import SemanticSplitterNodeParser
+from llama_index.storage.storage_context import StorageContext
+from llama_index.vector_stores import ChromaVectorStore
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 class TitleRag:
@@ -22,20 +25,24 @@ class TitleRag:
         embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
         transformations = [
             SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model),
-            # TitleExtractor(nodes=5, llm=llm),
-            # QuestionsAnsweredExtractor(questions=3, llm=llm),
-            # EntityExtractor(prediction_threshold=0.5),
-            # SummaryExtractor(summaries=["prev", "self", "next"], llm=llm),
-            # KeywordExtractor(keywords=10, llm=llm),
+            TitleExtractor(nodes=5, llm=llm),
+            QuestionsAnsweredExtractor(questions=3, llm=llm),
+            EntityExtractor(prediction_threshold=0.5),
+            SummaryExtractor(summaries=["prev", "self", "next"], llm=llm),
+            KeywordExtractor(keywords=10, llm=llm),
         ]
         service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, transformations=transformations)
         documents = SimpleDirectoryReader("../output_domain").load_data()
         documents = self.add_metadata(documents)
-        # print([document.metadata for document in documents])
-        pipeline = IngestionPipeline(transformations=transformations)
+        print([document.metadata for document in documents])
+        db = chromadb.PersistentClient(path="./chroma_db")
+        chroma_collection = db.get_or_create_collection("llama-2-ten")
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        pipeline = IngestionPipeline(transformations=transformations, num_workers=4, vector_store=vector_store)
         nodes = pipeline.run(documents=documents)
-        # print(nodes[0].metadata, nodes[2].metadata)
-        self.initialize_indexing(nodes, service_context)
+        print(nodes[0].metadata, nodes[2].metadata)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        self.initialize_indexing(nodes, service_context, storage_context)
 
     def add_metadata(self, documents):
         for document in documents:
@@ -53,9 +60,9 @@ class TitleRag:
                 document.metadata['Type'] = 'Republican'
         return documents
 
-    def initialize_indexing(self, nodes, service_context):
-        index = VectorStoreIndex(nodes=nodes, service_context=service_context)
-        self.initialize_query_engine(index)
+    def initialize_indexing(self, nodes, service_context, storage_context):
+        index = VectorStoreIndex(nodes=nodes, service_context=service_context, storage_context=storage_context)
+        # self.initialize_query_engine(index)
 
     def initialize_query_engine(self, index):
         self.query_engine = index.as_query_engine()
