07656078f6d4e5b2dd9807ad80234450c822d168
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-24 00:41:29 -0800

    added ingestion pipeline

--- src/crawler/rag_domain/rag.py
+++ src/crawler/rag_domain/rag.py
@@ -3,12 +3,13 @@ import logging
 import sys
 
 from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings import HuggingFaceEmbedding
 from llama_index.extractors import (EntityExtractor, KeywordExtractor,
                                     QuestionsAnsweredExtractor,
                                     SummaryExtractor, TitleExtractor)
+from llama_index.ingestion import IngestionPipeline
 from llama_index.llms import Ollama
 from llama_index.node_parser import SemanticSplitterNodeParser
-from llama_index.embeddings import HuggingFaceEmbedding
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -19,7 +20,7 @@ class TitleRag:
         self.cities = ['California', 'Texas', 'Utah', 'New York', 'Kansas', 'Maryland', 'Massachusetts', 'South Carolina', 'South Dakota', 'Washington']
         llm = Ollama(model="mistral")
         embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-        transformation = [
+        transformations = [
             SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model),
             TitleExtractor(nodes=5, llm=llm),
             QuestionsAnsweredExtractor(questions=3, llm=llm),
@@ -27,10 +28,13 @@ class TitleRag:
             SummaryExtractor(summaries=["prev", "self"], llm=llm),
             KeywordExtractor(keywords=10, llm=llm),
         ]
-        service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, transformations=transformation)
+        service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, transformations=transformations)
         documents = SimpleDirectoryReader("../output_domain").load_data()
         documents = self.add_metadata(documents)
         print([document.metadata for document in documents])
+        pipeline = IngestionPipeline(transformations=transformations)
+        nodes = pipeline.run(documents=documents)
+        print(nodes[0].metadata, nodes[2].metadata)
         # self.initialize_indexing(documents, service_context)
 
     def add_metadata(self, documents):
@@ -52,7 +56,7 @@ class TitleRag:
     def initialize_indexing(documents, service_context):
         index = VectorStoreIndex.from_documents(documents, service_context)
         self.initialize_query_engine(index)
-    
+
     def initialize_query_engine(self, index):
         self.query_engine = index.as_query_engine()
         self.pre_evaluation()
