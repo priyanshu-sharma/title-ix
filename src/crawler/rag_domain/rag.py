c542c2292400fdba488601a3facededb84de44d0
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-24 17:50:00 -0800

    added time and version update

--- src/crawler/rag_domain/rag.py
+++ src/crawler/rag_domain/rag.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import sys
+import time
 
 import chromadb
 from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
@@ -19,17 +20,19 @@ logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 class TitleRag:
     def __init__(self):
+        start = time.time()
         self.result = []
         self.cities = ['California']#, 'Texas', 'Utah', 'New York', 'Kansas', 'Maryland', 'Massachusetts', 'South Carolina', 'South Dakota', 'Washington']
         llm = Ollama(model="llama2-uncensored", request_timeout=1000)
         embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
         transformations = [
-            SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model),
-            TitleExtractor(nodes=5, llm=llm),
-            QuestionsAnsweredExtractor(questions=3, llm=llm),
-            EntityExtractor(prediction_threshold=0.5),
-            SummaryExtractor(summaries=["prev", "self", "next"], llm=llm),
-            KeywordExtractor(keywords=10, llm=llm),
+            SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model, num_workers=8),
+            TitleExtractor(nodes=5, llm=llm, num_workers=8),
+            QuestionsAnsweredExtractor(questions=3, llm=llm, num_workers=8),
+            EntityExtractor(prediction_threshold=0.5, num_workers=8),
+            SummaryExtractor(summaries=["prev", "self", "next"], llm=llm, num_workers=8),
+            KeywordExtractor(keywords=10, llm=llm, num_workers=8),
+            embed_model,
         ]
         service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, transformations=transformations)
         documents = SimpleDirectoryReader("../output_domain").load_data()
@@ -38,11 +41,13 @@ class TitleRag:
         db = chromadb.PersistentClient(path="./chroma_db")
         chroma_collection = db.get_or_create_collection("llama-2-ten")
         vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-        pipeline = IngestionPipeline(transformations=transformations)
-        nodes = pipeline.run(documents=documents)
+        pipeline = IngestionPipeline(transformations=transformations, vector_store=vector_store)
+        nodes = await pipeline.arun(documents=documents)
         print(nodes[0].metadata, nodes[2].metadata)
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         self.initialize_indexing(nodes, service_context, storage_context)
+        end = time.time()
+        print("Total Time taken".format(end - start))
 
     def add_metadata(self, documents):
         for document in documents:
