2b93c26602c6e83bd8104d94cf787e266abaff51
priyanshu-sharma <prynsh1112@gmail.com>
2024-01-31 15:08:33 -0800

    added vader matrices

--- src/crawler/test_domain/custom_transformation.py
+++ src/crawler/test_domain/custom_transformation.py
@@ -1,12 +1,26 @@
 from llama_index.schema import TransformComponent
 from textblob import TextBlob
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class TextBlobTransformation(TransformComponent):
     def __call__(self, nodes, **kwargs):
         for node in nodes:
             node_data = TextBlob(node.text)
-            node['textblob'] = {
+            node.metadata['textblob'] = {
                 'polarity': node_data.sentiment.polarity,
                 'subjectivity': node_data.sentiment.subjectivity
+            }
+        return nodes
+
+class VaderTransformation(TransformComponent):
+    def __call__(self, nodes, **kwargs):
+        sent_analyzer = SentimentIntensityAnalyzer()
+        for node in nodes:
+            node_data = sent_analyzer.polarity_scores(node.text)
+            node.metadata['vader'] = {
+                'negative': node_data.get('neg', None),
+                'positive': node_data.get('pos', None),
+                'neutral': node_data.get('neu', None),
+                'compound': vader_score.get('compound', None)
             }
         return nodes
--- src/crawler/test_domain/test_rag.py
+++ src/crawler/test_domain/test_rag.py
@@ -14,7 +14,7 @@ from llama_index.llms import Ollama
 from llama_index.node_parser import SemanticSplitterNodeParser
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores import ChromaVectorStore
-from test_domain.custom_transformation import TextBlobTransformation
+from custom_transformation import TextBlobTransformation, VaderTransformation
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -27,12 +27,13 @@ class TitleRagQA:
         embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
         transformations = [
             SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model, num_workers=8),
-            TitleExtractor(nodes=5, llm=llm, num_workers=8),
-            QuestionsAnsweredExtractor(questions=5, llm=llm, num_workers=8),
-            EntityExtractor(prediction_threshold=0.5, num_workers=8),
-            SummaryExtractor(summaries=["prev", "self", "next"], llm=llm, num_workers=8),
-            KeywordExtractor(keywords=10, llm=llm, num_workers=8),
+            # TitleExtractor(nodes=5, llm=llm, num_workers=8),
+            # QuestionsAnsweredExtractor(questions=5, llm=llm, num_workers=8),
+            # EntityExtractor(prediction_threshold=0.5, num_workers=8),
+            # SummaryExtractor(summaries=["prev", "self", "next"], llm=llm, num_workers=8),
+            # KeywordExtractor(keywords=10, llm=llm, num_workers=8),
             TextBlobTransformation(),
+            VaderTransformation(),
         ]
         # service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, transformations=transformations)
         documents = SimpleDirectoryReader("data/").load_data()
@@ -43,41 +44,41 @@ class TitleRagQA:
         pipeline = IngestionPipeline(transformations=transformations, vector_store=vector_store)
         self.nodes = pipeline.run(documents=documents)
         print(self.nodes[0].metadata.keys())
-        print(self.nodes[0]['textblob'])
+        print(self.nodes[0].metadata['textblob'])
         end = time.time()
         print("Total Time taken - {} seconds".format(end - start))
 
-    def extract_metadata(self):
-        for node in self.nodes:
-            file_path = node.metadata.get('file_path')
-            state = file_path.split('/')[-1].split('.')[0]
-            if state in ['california']:
-                if state not in self.result.keys():
-                    self.result[state] = {
-                        'document_titles': {node.metadata.get('document_title')},
-                        'question_answers': {node.metadata.get('questions_this_excerpt_can_answer')},
-                        'entities': [entity for entity in node.metadata.get('entities')],
-                        'next_section_summary': {node.metadata.get('next_section_summary')},
-                        'section_summary': {node.metadata.get('section_summary')},
-                        'keywords': {node.metadata.get('excerpt_keywords')},
-                    }
-                    self.result[state]['entities'] = set(self.result[state]['entities'])
-                else:
-                    self.result[state]['document_titles'].add(node.metadata.get('document_title'))
-                    self.result[state]['question_answers'].add(node.metadata.get('questions_this_excerpt_can_answer'))
-                    for entity in node.metadata.get('entities'):
-                        self.result[state]['entities'].add(entity)
-                    self.result[state]['next_section_summary'].add(node.metadata.get('next_section_summary'))
-                    self.result[state]['section_summary'].add(node.metadata.get('section_summary'))
-                    self.result[state]['keywords'].add(node.metadata.get('excerpt_keywords'))
-        self.result[state]['document_titles'] = list(self.result[state]['document_titles'])
-        self.result[state]['question_answers'] = list(self.result[state]['question_answers'])
-        self.result[state]['entities'] = list(self.result[state]['entities'])
-        self.result[state]['next_section_summary'] = list(self.result[state]['next_section_summary'])
-        self.result[state]['section_summary'] = list(self.result[state]['section_summary'])
-        self.result[state]['keywords'] = list(self.result[state]['keywords'])
-        with open("california_qa.json", "w") as f:
-            json.dump(self.result, f)
+    # def extract_metadata(self):
+    #     for node in self.nodes:
+    #         file_path = node.metadata.get('file_path')
+    #         state = file_path.split('/')[-1].split('.')[0]
+    #         if state in ['california']:
+    #             if state not in self.result.keys():
+    #                 self.result[state] = {
+    #                     'document_titles': {node.metadata.get('document_title')},
+    #                     'question_answers': {node.metadata.get('questions_this_excerpt_can_answer')},
+    #                     'entities': [entity for entity in node.metadata.get('entities')],
+    #                     'next_section_summary': {node.metadata.get('next_section_summary')},
+    #                     'section_summary': {node.metadata.get('section_summary')},
+    #                     'keywords': {node.metadata.get('excerpt_keywords')},
+    #                 }
+    #                 self.result[state]['entities'] = set(self.result[state]['entities'])
+    #             else:
+    #                 self.result[state]['document_titles'].add(node.metadata.get('document_title'))
+    #                 self.result[state]['question_answers'].add(node.metadata.get('questions_this_excerpt_can_answer'))
+    #                 for entity in node.metadata.get('entities'):
+    #                     self.result[state]['entities'].add(entity)
+    #                 self.result[state]['next_section_summary'].add(node.metadata.get('next_section_summary'))
+    #                 self.result[state]['section_summary'].add(node.metadata.get('section_summary'))
+    #                 self.result[state]['keywords'].add(node.metadata.get('excerpt_keywords'))
+    #     self.result[state]['document_titles'] = list(self.result[state]['document_titles'])
+    #     self.result[state]['question_answers'] = list(self.result[state]['question_answers'])
+    #     self.result[state]['entities'] = list(self.result[state]['entities'])
+    #     self.result[state]['next_section_summary'] = list(self.result[state]['next_section_summary'])
+    #     self.result[state]['section_summary'] = list(self.result[state]['section_summary'])
+    #     self.result[state]['keywords'] = list(self.result[state]['keywords'])
+    #     with open("california_qa.json", "w") as f:
+    #         json.dump(self.result, f)
 
 ta = TitleRagQA()
-ta.extract_metadata()
+# ta.extract_metadata()
