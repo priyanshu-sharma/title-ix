862b811b01400f01d6bb11d042dd941796358aef
priyanshu-sharma <prynsh1112@gmail.com>
2024-02-06 15:04:47 -0800

    fix min cluster

--- src/crawler/topic_domain/topic.py
+++ src/crawler/topic_domain/topic.py
@@ -25,7 +25,7 @@ class TopicDistribution:
         self.result = []
         start = time.time()
         transformations = [
-            SentenceSplitter(chunk_size=512, chunk_overlap=20)
+            SentenceSplitter(chunk_size=1024, chunk_overlap=20)
         ]
         documents = SimpleDirectoryReader("../output_domain").load_data()
         pipeline = IngestionPipeline(transformations=transformations)
@@ -47,11 +47,11 @@ class TopicDistribution:
         embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
         embeddings = embedding_model.encode(self.texts, show_progress_bar=True)
         umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine', random_state=42)
-        hdbscan_model = HDBSCAN(min_cluster_size=200, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
-        vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 4))
+        hdbscan_model = HDBSCAN(min_cluster_size=150, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+        vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 5))
         keybert_model = KeyBERTInspired(top_n_words=30)
-        mmr_model = MaximalMarginalRelevance(diversity=0.5)
-        combined_model = [KeyBERTInspired(top_n_words=30), MaximalMarginalRelevance(diversity=0.5)]
+        mmr_model = MaximalMarginalRelevance(diversity=0.3)
+        combined_model = [KeyBERTInspired(top_n_words=30), MaximalMarginalRelevance(diversity=0.3)]
         model = AutoModelForCausalLM.from_pretrained(
             "TheBloke/zephyr-7B-alpha-GGUF",
             model_file="zephyr-7b-alpha.Q4_K_M.gguf",
