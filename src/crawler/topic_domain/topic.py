161a3c29026b38b5fac34e7fd6cdb19b8103a4c8
priyanshu-sharma <prynsh1112@gmail.com>
2024-02-06 16:00:36 -0800

    added basic config

--- src/crawler/topic_domain/topic.py
+++ src/crawler/topic_domain/topic.py
@@ -40,11 +40,11 @@ class TopicDistribution:
             self.texts.append(node.text)
 
     def configure_embedding(self):
-        embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-        embeddings = embedding_model.encode(self.texts, show_progress_bar=True)
-        umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine', random_state=42)
-        hdbscan_model = HDBSCAN(min_cluster_size=150, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
-        vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 5))
+        # embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+        # embeddings = embedding_model.encode(self.texts, show_progress_bar=True)
+        # umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine', random_state=42)
+        # hdbscan_model = HDBSCAN(min_cluster_size=100, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+        # vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 5))
         keybert_model = KeyBERTInspired(top_n_words=30)
         mmr_model = MaximalMarginalRelevance(diversity=0.3)
         combined_model = [KeyBERTInspired(top_n_words=30), MaximalMarginalRelevance(diversity=0.3)]
@@ -54,12 +54,12 @@ class TopicDistribution:
             "combined": combined_model,
         }
         self.topic_model = BERTopic(
-            embedding_model=embedding_model,
-            umap_model=umap_model,
-            hdbscan_model=hdbscan_model,
-            vectorizer_model=vectorizer_model,
+        #     embedding_model=embedding_model,
+        #     umap_model=umap_model,
+        #     hdbscan_model=hdbscan_model,
+        #     vectorizer_model=vectorizer_model,
             representation_model=representation_model,
-            top_n_words=10,
+            # top_n_words=10,
             verbose=True
         )
         topics, probs = self.topic_model.fit_transform(self.texts, embeddings)
