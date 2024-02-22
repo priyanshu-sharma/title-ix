c206644fe8088b16fbdba132f69a630e21578add
priyanshu-sharma <prynsh1112@gmail.com>
2024-02-22 01:11:55 -0800

    change model to mistral

--- src/crawler/datarag/scoring.py
+++ src/crawler/datarag/scoring.py
@@ -15,7 +15,7 @@ for state in states:
         content = '{}\nHere is the response for the above scenario - {}\nCan you rate this information based on the help provided by the generated response on a scale of 0-10, where 0 is for lowest level of information, while 10 for the most information. Only provide the score nothing else. No extra information or explaination.'.format(
             question, answer
         )
-        response = ollama.chat(model='llama2', messages=[{'role': 'user', 'content': content,},])
+        response = ollama.chat(model='mistral', messages=[{'role': 'user', 'content': content,},])
         result = response['message']['content']
         response_list.append({'Question': question, 'Response': answer, 'Scoring': result})
     with open("../datadump/{}_scoring.json".format(state), "w") as f:
